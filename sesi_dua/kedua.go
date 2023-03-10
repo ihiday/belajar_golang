fbeceb4e32d56341438f68d9ea6dc833d45ba4fd
Your Name <you@example.com>
2023-03-10 19:14:11 +0700

    mencoba tugas dua

--- sesi_dua/kedua.go
+++ sesi_dua/kedua.go
@@ -3,6 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("halodunia")
+	for i := 0; i <= 4; i++ {
+		fmt.Println("Nilai dari i adalah ", i)
+	}
+	for j := 0; j <= 10; j++ {
+		if j == 5 {
+			teks := "hai"
+			for indeks := 0; indeks < len(teks); indeks++ {
+				fmt.Printf("Characters %U starts at byte position %d \n", rune(teks[indeks]), indeks*2)
+			}
+		} else {
+			fmt.Println("Nilai dari j adalah ", j)
+		}
+	}
 
 }
