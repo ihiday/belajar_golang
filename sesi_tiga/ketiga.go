f0a9f9393d3a637a05882174d7ee25cf3e6303f7
Your Name <you@example.com>
2023-03-11 00:40:04 +0700

    menambahkan komentar

--- sesi_tiga/ketiga.go
+++ sesi_tiga/ketiga.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+
 	// deklarasi kalimat
 	str := "selamat malam"
 
@@ -10,13 +11,14 @@ func main() {
 	data := map[string]int{}
 
 	for i := 0; i < len(str); i++ {
+
 		// deklarasi untuk memudahkan memasukkan key
 		karakter := string(str[i])
 
 		// menampilkan karakter
 		fmt.Println(karakter)
-		if string(str[i]) == karakter {
 
+		if string(str[i]) == karakter {
 			// melakukan input dan penambahan nilai jika karakter sama
 			data[karakter] += 1
 		}
