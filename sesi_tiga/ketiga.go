56c7f169f99c3bf09a35c4f770b26acb0e3ae7aa
Your Name <you@example.com>
2023-03-11 00:38:31 +0700

    menambahkan komentar

--- sesi_tiga/ketiga.go
+++ sesi_tiga/ketiga.go
@@ -10,9 +10,14 @@ func main() {
 	data := map[string]int{}
 
 	for i := 0; i < len(str); i++ {
+		// deklarasi untuk memudahkan memasukkan key
 		karakter := string(str[i])
+
+		// menampilkan karakter
 		fmt.Println(karakter)
 		if string(str[i]) == karakter {
+
+			// melakukan input dan penambahan nilai jika karakter sama
 			data[karakter] += 1
 		}
 	}
