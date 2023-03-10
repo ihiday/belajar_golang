c22f623921aa9ce44640cbffd7784b4cf0cae6e9
Your Name <you@example.com>
2023-03-10 20:55:42 +0700

    final tugas 2

--- sesi_dua/kedua.go
+++ sesi_dua/kedua.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	for i := 0; i <= 4; i++ {
-		fmt.Println("Nilai dari i adalah ", i)
+		fmt.Println("Nilai i = ", i)
 	}
 	for j := 0; j <= 10; j++ {
 		if j == 5 {
@@ -13,7 +13,7 @@ func main() {
 				fmt.Printf("character %U '%s' starts at byte position %d \n", teks[indeks], string(teks[indeks]), indeks*2)
 			}
 		} else {
-			fmt.Println("Nilai dari j adalah ", j)
+			fmt.Println("Nilai j = ", j)
 		}
 	}
 
