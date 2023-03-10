37aa3ba210a240de5094f70b97841851c8f89220
Your Name <you@example.com>
2023-03-11 00:35:51 +0700

    menambahkan komentar

--- sesi_dua/kedua.go
+++ sesi_dua/kedua.go
@@ -3,13 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	// perulangan nilai i
 	for i := 0; i <= 4; i++ {
 		fmt.Println("Nilai i = ", i)
 	}
+
+	// perulangan nilai j
 	for j := 0; j <= 10; j++ {
 		if j == 5 {
+
+			// deklarasi teks untuk perulangan
 			teks := []rune{'С', 'А', 'Ш', 'А', 'Р', 'В', 'О'}
-			// teks := "САШАРВО"
+
+			// perulangan karakter
 			for indeks := 0; indeks < len(teks); indeks++ {
 				char := teks[indeks]
 				fmt.Printf("character %U '%s' starts at byte position %d \n", char, string(char), indeks*2)
